f5901a9e35280e7524f172988be4b4492239b415
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-09-26 17:09:29 +0300

    feat: added footer links

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,37 @@ import { Control, Grid } from './features';
 
 const { Header, Footer, Content } = Layout;
 
-function App() {
+const FOOTER_LINKS = {
+  code: {
+    link: 'https://github.com/alexeikravchuk/task1',
+    title: 'Исходный код',
+  },
+  projects: {
+    link: 'https://alexeikravchuk.github.io/rsschool-cv/#projects',
+    title: 'Выполненные работы',
+  },
+};
 
-  return (
-    <Layout className="App">
-      <Header>
-        <PageHeader
-          className="site-page-header"
-          title="2D сетка с координатами объектов (точек) по x,y."
-          subTitle=" (Декартова система)"
-        />
-      </Header>
-      <Content className="content">
-        <Grid />
-        <Control />
-      </Content>
-      <Footer>Footer</Footer>
-    </Layout>
-  );
-}
+const App: React.FC = () => (
+  <Layout className="App">
+    <Header>
+      <PageHeader
+        className="site-page-header"
+        title="2D сетка с координатами объектов по x, y. "
+        subTitle=" (Декартовая система)"
+      />
+    </Header>
+    <Content className="content">
+      <Grid />
+      <Control />
+    </Content>
+    <Footer className="footer">
+      <div className="footer-container">
+        <a href={FOOTER_LINKS.code.link} className="github" target="blank">{FOOTER_LINKS.code.title}</a>
+        <a href={FOOTER_LINKS.projects.link} className="github" target="blank">{FOOTER_LINKS.projects.title}</a>
+      </div>
+    </Footer>
+  </Layout>
+);
 
 export default App;
